4dc62cb2d827495040c89c7def359b687a7150ea
yoshihisat <44428478+yoshihisat@users.noreply.github.com>
2020-12-17 10:16:14 +0900

    自分のおける場所の表示

---
+++ Othello_CanPutDown.py
@@ -0,0 +1,37 @@
+#Othello
+
+class CanPutDown():
+# #黒色が先行白色が後攻
+# # ボートインスタンスの作成
+# board = Board()
+ 
+# # 手番ループ
+# while True:
+ 
+#     # 盤面の表示
+#     board.display()
+ 
+#     # 手番の表示
+#     if board.CurrentColor == BLACK:
+#         print('黒の番です:', end = "")
+#     else:
+#         print('白の番です:', end = "")
+#     IN = input()
+#     print()
+ 
+#     # 入力手をチェック
+#     if board.checkIN(IN):
+#         x = IN_ALPHABET.index(IN[0]) + 1
+#         y = IN_NUMBER.index(IN[1]) + 1
+#     else:
+#         print('正しい形式で入力してください')
+#         continue
+ 
+#     # 手を打つ
+#     if not board.move(x, y):
+#         print('そこには置けません')
+#         continue
+    
+
+    def __init__(self):
+
